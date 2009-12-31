68f6d433c561a5f70f4c1a5a333c37f259d56b70
Andy Kilner <andy@andykilner.co.uk>
2009-12-31 11:01:16 +0000

    Initial version.
    Currently it lists the top track albums you probably own (and have therefore
    played in full).

---
+++ albumblr.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import logging
+from optparse import OptionParser
+
+import pylast
+
+from config import *
+
+def find_albums(api, username):
+    user = api.get_user(username)
+    lib = user.get_library()
+    albums = {}
+
+    top_tracks = user.get_top_tracks()
+    for track in top_tracks:
+        album = track.item.get_album()
+        logging.debug("Adding top track '%s' album '%s'" % (track.item, album))
+        if album:
+            albums[album.get_mbid()] = album
+        else:
+            logging.info("No album for track '%s'" % track.item)
+
+    for mbid, album in albums.items():
+        logging.info("Looking for tracks on '%s'" % album)
+        album_tracks = album.get_tracks()
+        have_tracks  = lib.get_tracks(
+                artist = album.artist.name,
+                album  = album.title)
+        album_len, have_len = len(album_tracks), len(have_tracks)
+        logging.info("    found %d of %d tracks" % (have_len, album_len))
+        if (album_len - have_len) > 1:
+            logging.debug("Removing album '%s'" % album)
+            del albums[mbid]
+        else:
+            yield album
+
+def main(username):
+    api = pylast.get_lastfm_network(api_key=API_KEY, api_secret=API_SECRET)
+    albums = find_albums(api, username)
+    for album in albums:
+        print album
+
+def add_options(parser):
+    parser.add_option('-d', '--debug',
+            action = 'store_true')
+    parser.add_option('-v', '--verbose',
+            action = 'store_true')
+
+    def post_func(opts, args):
+        if len(args) != 1:
+            parser.error("No username given")
+
+def init_logging(opts):
+    if opts.debug:
+        log_level = logging.DEBUG
+    elif opts.verbose:
+        log_level = logging.INFO
+    else:
+        log_level = logging.WARN
+    logging.basicConfig(level=log_level)
+
+if __name__ == '__main__':
+    usage = "usage: %prog [options] USERNAME"
+    parser = OptionParser(usage)
+    post_func = add_options(parser)
+    opts, args = parser.parse_args()
+    if post_func:
+        post_func(opts, args)
+    init_logging(opts)
+
+    main(*args)
